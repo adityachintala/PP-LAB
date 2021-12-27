e6bdab9f8ae93f92200227274b4aad87c34db663
Adithya K <a.dty.3403@gmail.com>
2021-12-27 21:27:55 +0530

    updated

--- restaurant.py
+++ restaurant.py
@@ -1,39 +1,33 @@
-"""
-Make a class called Restaurant. The __init__() method for Restaurant should store two attributes:  a restaurant name and a cuisine type. Make a method called describe_restaurant() that prints these two pieces of information, and a method called open_restaurant() that prints a message indicating that the restaurant is open. Make an instance called restaurant from your class. and then call both methods.
-
-Note:
-if the time is in between 11hrs and 23hrs it should print 
-    Restaurant Open
-"""
+# Make a class called Restaurant. The init () method for Restaurant should store two attributes: a restaurant_name
+# and a cuisine_type. Make a method called describe_restaurant() that prints these two pieces of information, and a
+# method called open_restaurant() that prints a message indicating that the restaurant is open. Create three
+# different instances from the class, print the two attributes individually, and then call both methods for
+# each instance.
 
 from datetime import datetime
 
 
-class Restaurant():
+class Restaurant:
     def __init__(self, name, type):
         self.name = name
         self.type = type
 
-    def describe_restaurant(self):
-        print("Welcome to " + self.name + "\n" + self.type)
+    def describe(self):
+        print("Restaurant name :", self.name)
+        print("Cuisine type :", self.type)
 
-    def open_restaurant(self):
-        now = datetime.now()
-        if now.hour >= 11 and now.hour <= 23:
-            print("Restaurant Open")
+    def open(self):
+        time = datetime.now()
+        hr = int(time.strftime('%H'))
+        if (hr >= 11 and hr <= 23):
+            print("Open")
         else:
-            print("Restaurant Closed")
+            print("Closed")
 
 
-p = input("Enter the Restaurant name: ")
-d = input("Enter the Dish: ")
-r = Restaurant(p, d)
-r.describe_restaurant()
-r.open_restaurant()
-
-
-# Enter the Restaurant name: paradise
-# Enter the Dish: biryani
-# Welcome to paradise
-# biryani
-# Restaurant Open
+r1 = Restaurant("Pizza Hut", "Pizza")
+r1.describe()
+r1.open()
+r2 = Restaurant("McDonalds", "Fast Food")
+r2.describe()
+r2.open()
