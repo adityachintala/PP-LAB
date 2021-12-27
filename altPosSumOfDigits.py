a97966e72698cb55fbd176eab4e3aea09ba9e32d
Adithya K <a.dty.3403@gmail.com>
2021-12-27 21:29:52 +0530

    updated

--- altPosSumOfDigits.py
+++ altPosSumOfDigits.py
@@ -1,37 +1,13 @@
-"""
-Given a positive integer 'x' (with even number of digits in it), compute the difference between the sum of the digits occurring in the alternate positions (starting from the first position) and the sum of the digits occurring in the alternate positions, starting from the last rightmost position of 'x'.
-"""
+# Given a positive integer 'x' (with even number of digits in it), compute the difference between the sum of the
+# digits occurring in the alternate positions (starting from the first position) and the sum of the digits occurring in the
+# alternate positions, starting from the last rightmost position of 'x'.
 
-
-def reverse(n):
-    rev = 0
-    while (n != 0):
-        rev = (rev * 10) + (n % 10)
-        n //= 10
-    return rev
-
-
-def getSum(n):
-    n = reverse(n)
-    sumOdd = 0
-    sumEven = 0
-    c = 1
-    while (n != 0):
-        if (c % 2 == 0):
-            sumEven += n % 10
-        else:
-            sumOdd += n % 10
-        n //= 10
-        c += 1
-    print("Odd :", sumOdd)
-    print("Even :", sumEven)
-    print("Difference :", sumOdd - sumEven)
-
-
-x = int(input("Enter a number : "))
-getSum(x)
-
-# Enter a number : 142536
-# Odd : 6
-# Even : 15
-# Difference : -9
+n = input("Enter number : ")
+if len(n) % 2 != 0 or int(n) <= 0:
+    print("Invalid")
+else:
+    s1, s2 = 0, 0
+    for i in range(0, len(n), 2):
+        s1 += int(n[i])
+        s2 += int(n[i + 1])
+    print(abs(s1 - s2))
