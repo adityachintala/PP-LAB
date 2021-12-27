f396772091d350ab3d5341ff804659d0b76767ee
Adithya K <a.dty.3403@gmail.com>
2021-12-27 21:27:22 +0530

    updated

--- atm_and_robot.py
+++ atm_and_robot.py
@@ -1,49 +1,41 @@
-"""
-Write the following classes with class variables, instance variable and illustration the self variable
-i) Robot (to greet the world)
-ii) ATM (to deposit and withdraw amount from ATM machine)
-"""
+# Write the following classes with class variables, instance variable and illustration the self variable
+# i) Robot (to greet the world)
+# ii) ATM (to deposit and withdraw amount from ATM machine)
 
 
-class ATM:
-    balance = 0
-
-    def __init__(self, amount):
-        self.balance = amount
-
-    def withdraw(self, amount):
-        self.balance -= amount
-        return self.balance
-
-    def deposit(self, amount):
-        self.balance += amount
-        return self.balance
-
-
+# class Robot
 class Robot:
     def __init__(self, name):
         self.name = name
 
     def greet(self):
-        print("Robot says hello to", self.name)
+        print("Hello, I am " + self.name)
 
 
-# Robot
-str = input("Enter your name:")
-r = Robot(str)
+name = input("Enter name :")
+r = Robot(name)
 r.greet()
-# ATM
-atm = ATM(50000)
-print("Balance =", 50000)
-n = int(input("Enter the amount to withdraw: "))
-print("Remaining balance: ", atm.withdraw(n))
-n = int(input("Enter the amount to deposit: "))
-print("Updated balance: ", atm.deposit(n))
 
-# Enter your name:john
-# Robot says hello to john
-# Balance = 50000
-# Enter the amount to withdraw: 5000
-# Remaining balance:  45000
-# Enter the amount to deposit: 200
-# Updated balance:  45200
+
+# class ATM
+class ATM:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited amount :", amount)
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient balance")
+        else:
+            self.balance -= amount
+            print("Withdrawn amount : ", amount)
+
+
+balance = int(input("Enter balance : "))
+a = ATM(balance)
+a.deposit(int(input("Enter amount to deposit : ")))
+a.withdraw(int(input("Enter amount to withdraw : ")))
+print("Balance :", a.balance)
