2c60638b1b8dc49a0dd6f24e9fd2f12c98c10744
Adithya K <a.dty.3403@gmail.com>
2021-12-27 21:30:14 +0530

    updated

--- tuplesCubes.py
+++ tuplesCubes.py
@@ -1,12 +1,8 @@
 # Write a program to create a list of tuples from given list having number and its cube in each tuple.
 
-list = [1, 2, 3, 4, 5]
-list2 = []
-tuple = ()
-for i in list:
-    tuple = (i, i**3)
-    list2.append(tuple)
-print(list2)
-
-
-# [(1, 1), (2, 8), (3, 27), (4, 64), (5, 125)]
+nums = input("Enter numbers : ")
+nums = list(map(int, nums.split()))
+l = []
+for i in nums:
+    l.append((i, i**3))
+print(l)
