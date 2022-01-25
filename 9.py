58aa06823a83d292477e4f125d09395a54337de9
Adithya K <a.dty.3403@gmail.com>
2022-01-25 11:18:36 +0530

    updated

--- 9.py
+++ 9.py
@@ -1,11 +1,11 @@
 # Write a program to print each line of a file in reverse order. Also compute the number of characters, words and lines in a file.
 
-file = open("adty.txt", "r")
+file = open("sample.txt", "r")
 lines = file.readlines()
 file.close()
 for line in lines:
     print(line[::-1])
-file = open("adty.txt", "r")
+file = open("sample.txt", "r")
 lines = file.readlines()
 file.close()
 words = 0
